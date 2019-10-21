9523313638dd12adb103f7d5243ce92041338dda
Nicholas Hemley <nicholas.oliver.hemley@gmail.com>
2019-10-21 13:48:44 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+# focus-sensors
+
+## Quickstart
+
+Assumes a *nix based machine.
+
+## To setup Kafka on a local machine
+
+1. Download Kafka from https://kafka.apache.org/downloads.
+1. Follow steps 1, 2 and 3 from https://kafka.apache.org/quickstart.  I.e. Untar kafka, start the server and create a Kafka topic.
+
+## Running the code
+
+1. Create a Python virtual environment: `python3 -m venv env`.
+1. Activate the virtual environment: `source env/bin/activate`.
+1. Install dependencies: `pip install -r requirements.txt`.
+1. Set the variables as required at the top of the `producer.py` and `consumer.py` files.
+1. Start the consumer: `python consumer.py`.
+1. Start the producer: `python producer.py`.
+
+## To cleanup Kafka topic
+
+You need to do this if playing with the code many times as it will eat up disk space quickly!
+
+1. `./bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic test`
+1. Stop Kafka and Zookeeper then run `rm -rf /tmp/kafka-logs/`.
+1. Start Kafka and Zookeeper then re-create the topic, e.g. 
+   `bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test`.
+   
+## Documentation
+
+The documentation for the Python Kafka API can be found here:
+
+* https://github.com/confluentinc/confluent-kafka-python.
+
---
+++ consumer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+
+from confluent_kafka import Consumer
+
+
+###
+### PLEASE SET THE BELOW CONFIGURATION
+###
+
+# Address of the kafka servers and topic name
+kafka_servers = 'localhost:9092'
+topic_name = 'test'
+
+# Whether to only listen for messages that occurred since the consumer started ('latest'),
+# or to pick up all messages that the consumer has missed ('earliest').
+# Using 'latest' means the consumer must be started before the producer.
+read_topic_from = 'latest'
+
+# How often to indicate data rate in seconds
+throughput_debug_interval_in_sec = 1
+
+###
+### Consumer code
+###
+
+kbs_in_mb = 1000
+
+c = Consumer({
+    'bootstrap.servers': kafka_servers,
+    'group.id': 'mygroup',
+    'auto.offset.reset': read_topic_from
+})
+
+c.subscribe([topic_name])
+
+kbs_so_far = 0
+
+window_start_time = int(time.time())
+
+while True:
+    
+    # Waits 1 second to receive a message, if it doesn't find one goes round the loop again
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+    
+    current_time = int(time.time())
+            
+    # Maintain figures for throughput reporting
+    kbs_so_far += sys.getsizeof(msg.value())/1000
+    
+    # Determine if we should output a throughput figure
+    window_length_sec = current_time - window_start_time
+    
+    if window_length_sec >= throughput_debug_interval_in_sec:
+        print('Throughput in window: {} MB/s'.format(
+                int(kbs_so_far / (throughput_debug_interval_in_sec*kbs_in_mb))))
+        # Reset ready for the next throughput indication
+        window_start_time = int(time.time())
+        kbs_so_far = 0
+    
+
+c.close()
