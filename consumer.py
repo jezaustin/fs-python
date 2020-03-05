ee35a91d050d4e3ecf50f3b365c7736fb9a82ca4
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-05 13:34:02 +0000

    Changed reporting time to 5s.

--- consumer.py
+++ consumer.py
@@ -18,7 +18,7 @@ endpoint_url=os.environ["ENDPOINT_URL"] or "print"
 
 # Address of the kafka servers and topic name
 #kafka_servers = '192.168.56.101:9092'
-kafka_servers = 'internal-service-0.kafka.svc.cluster.local:32400,internal-service-1.kafka.svc.cluster.local:32401,internal-service-2.kafka.svc.cluster.local:32402'
+kafka_servers = 'internal-service-0.kafka.svc.cluster.local:32400'
 topic_name = 'test'
 
 # Whether to only listen for messages that occurred since the consumer started ('latest'),
@@ -27,7 +27,7 @@ topic_name = 'test'
 read_topic_from = 'latest'
 
 # How often to indicate data rate in seconds
-throughput_debug_interval_in_sec = 1
+throughput_debug_interval_in_sec = 5 
 
 ###
 ### Consumer code
